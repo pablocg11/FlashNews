f78cb19694edfe3117875e0cf07358e7675938a3
Pablo Castro <pablocastrodb@gmail.com>
2024-10-29 17:27:45 +0100

    Initial Commit

---
+++ FlashNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlashNews
+//
+//  Created by Pablo Castro on 29/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlashNews/FlashNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashNewsApp.swift
+//  FlashNews
+//
+//  Created by Pablo Castro on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
